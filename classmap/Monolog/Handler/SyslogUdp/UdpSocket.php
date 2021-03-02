1b420c5cdfe9ffd479016d85f7758f975a738b28
hdj <hdj@addcn.com>
2021-03-02 15:39:40 +0800

    first commit

---
+++ classmap/Monolog/Handler/SyslogUdp/UdpSocket.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of tw8591-im-service.
+ *
+ * @link     https://code.addcn.com/8591/services/im
+ * @document https://code.addcn.com/8591/services/im/-/blob/master/README.md
+ * @contact  hdj@addcn.com
+ */
+namespace Monolog\Handler\SyslogUdp;
+
+use Monolog\Utils;
+use Swoole\Coroutine\Client;
+
+class UdpSocket
+{
+    protected const DATAGRAM_MAX_LENGTH = 65023;
+
+    /** @var string */
+    protected $ip;
+
+    /** @var int */
+    protected $port;
+
+    /**
+     * @var Client
+     */
+    protected $socket;
+
+    public function __construct(string $ip, int $port = 514)
+    {
+        $this->ip = $ip;
+        $this->port = $port;
+    }
+
+    public function write($line, $header = '')
+    {
+        $this->send($this->assembleMessage($line, $header));
+    }
+
+    public function close(): void
+    {
+        // todo
+    }
+
+    protected function send(string $chunk): void
+    {
+        co(function () use ($chunk) {
+            $socket = new Client(SWOOLE_SOCK_UDP);
+            $socket->connect($this->ip, $this->port, 0.5);
+            defer(function () use ($socket) {
+                $socket->close();
+            });
+            $socket->send($chunk);
+        });
+    }
+
+    protected function assembleMessage(string $line, string $header): string
+    {
+        $chunkSize = static::DATAGRAM_MAX_LENGTH - strlen($header);
+
+        return $header . Utils::substr($line, 0, $chunkSize);
+    }
+}
---
+++ src/ConfigProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of monolog-hook.
+ *
+ * @link     https://github.com/huangdijia/monolog-hook
+ * @document https://github.com/huangdijia/monolog-hook/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\MonologHook;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        defined('BASE_PATH') or define('BASE_PATH', '');
+
+        return [
+            'dependencies' => [],
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+                'class_map' => [
+                    Monolog\Handler\SyslogUdp\UdpSocket::class => __DIR__ . '/../classmap/Monolog/Handler/SyslogUdp/UdpSocket.php',
+                ],
+            ],
+            'commands' => [],
+            'listeners' => [],
+            'publish' => [],
+        ];
+    }
+}
