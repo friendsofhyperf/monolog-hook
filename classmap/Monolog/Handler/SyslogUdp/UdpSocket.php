63b82a899266744e676b55f4c8a07eaf0a0858bc
Deeka Wong <huangdijia@gmail.com>
2022-06-29 14:31:07 +0800

    Compact non-coroutinue

--- classmap/Monolog/Handler/SyslogUdp/UdpSocket.php
+++ classmap/Monolog/Handler/SyslogUdp/UdpSocket.php
@@ -10,7 +10,9 @@ declare(strict_types=1);
  */
 namespace Monolog\Handler\SyslogUdp;
 
+use Hyperf\Utils\Coroutine;
 use Monolog\Utils;
+use Socket;
 use Swoole\Coroutine\Client;
 
 class UdpSocket
@@ -23,9 +25,7 @@ class UdpSocket
     /** @var int */
     protected $port;
 
-    /**
-     * @var Client
-     */
+    /** @var null|resource|Socket */
     protected $socket;
 
     public function __construct(string $ip, int $port = 514)
@@ -41,19 +41,57 @@ class UdpSocket
 
     public function close(): void
     {
-        // todo
+        if (Coroutine::inCoroutine()) {
+            return;
+        }
+
+        if (is_resource($this->socket) || $this->socket instanceof Socket) {
+            socket_close($this->socket);
+            $this->socket = null;
+        }
+    }
+
+    /**
+     * @return resource|Socket
+     */
+    protected function getSocket()
+    {
+        if ($this->socket !== null) {
+            return $this->socket;
+        }
+
+        $domain = AF_INET;
+        $protocol = SOL_UDP;
+        // Check if we are using unix sockets.
+        if ($this->port === 0) {
+            $domain = AF_UNIX;
+            $protocol = IPPROTO_IP;
+        }
+
+        $this->socket = socket_create($domain, SOCK_DGRAM, $protocol) ?: null;
+        if ($this->socket === null) {
+            throw new \RuntimeException('The UdpSocket to ' . $this->ip . ':' . $this->port . ' could not be opened via socket_create');
+        }
+
+        return $this->socket;
     }
 
     protected function send(string $chunk): void
     {
-        co(function () use ($chunk) {
-            $socket = new Client(SWOOLE_SOCK_UDP);
-            $socket->connect($this->ip, $this->port, 0.5);
-            defer(function () use ($socket) {
-                $socket->close();
+        if (Coroutine::inCoroutine()) {
+            co(function () use ($chunk) {
+                $socket = new Client(SWOOLE_SOCK_UDP);
+                $socket->connect($this->ip, $this->port, 0.5);
+                defer(function () use ($socket) {
+                    $socket->close();
+                });
+                $socket->send($chunk);
             });
-            $socket->send($chunk);
-        });
+
+            return;
+        }
+
+        socket_sendto($this->getSocket(), $chunk, strlen($chunk), $flags = 0, $this->ip, $this->port);
     }
 
     protected function assembleMessage(string $line, string $header): string
